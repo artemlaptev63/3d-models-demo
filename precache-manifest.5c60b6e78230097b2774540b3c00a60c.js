89df3eb2d24e26205708a864ac45aaf082a7459b
Artem Laptev <artemlaptev63@mail.ru>
2020-06-09 19:28:26 +0400

    Updates

---
+++ precache-manifest.5c60b6e78230097b2774540b3c00a60c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417a4e18d6de12e5aa6fd728e18acfc9",
+    "url": "/3d-models-demo/index.html"
+  },
+  {
+    "revision": "06d06df68464dadd89e5",
+    "url": "/3d-models-demo/static/css/main.a70c2b96.chunk.css"
+  },
+  {
+    "revision": "0b8ecbbb94c4a0b144ae",
+    "url": "/3d-models-demo/static/js/2.39aa74c1.chunk.js"
+  },
+  {
+    "revision": "2739a4eff10b3d58a801a5c208531cfb",
+    "url": "/3d-models-demo/static/js/2.39aa74c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d06df68464dadd89e5",
+    "url": "/3d-models-demo/static/js/main.a3460ea3.chunk.js"
+  },
+  {
+    "revision": "aee731e9dd523358498d",
+    "url": "/3d-models-demo/static/js/runtime-main.d0c45e4e.js"
+  }
+]);
